90e1996d23850b3e31b068f48ece6ffacd3cab09
Peter Rifkind <prifkind@gmail.com>
2021-12-12 09:48:21 -0500

    completed tiling of returned images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,13 @@
 import React from "react";
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return (
-      <img alt={description} src={urls.regular} key={id} />
-    );
+  const images = props.images.map((image) => {
+    return <ImageCard image={image} key={image.id} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
